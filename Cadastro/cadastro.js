d3a24df0fdc1d173334332cca6d4dcc9d67465ab
Olavo-dev <olavo.arruda@aluno.senai.br>
2025-05-07 20:06:29 -0300

    ADD LINK PAG

--- Cadastro/cadastro.js
+++ Cadastro/cadastro.js
@@ -11,18 +11,32 @@ loginButton.onclick =  () => {
 cadastroButton.onclick =  () => {
     card.classList.remove("loginActive");
     card.classList.add("cadastroActive");
-   
 }
 
 const barraPesquisa = document.getElementById("barraPesquisa");
 const lista = document.getElementById("listaDeItens");
-const itens = lista.getElementsByTagName("li");
 
-barraPesquisa.addEventListener("input", function () {
-  const texto = barraPesquisa.value.toLowerCase();
+if (barraPesquisa && lista) {
+  const itens = lista.getElementsByTagName("li");
 
-  for (let i = 0; i < itens.length; i++) {
-    const item = itens[i].textContent.toLowerCase();
-    itens[i].style.display = item.includes(texto) ? "block" : "none";
+  barraPesquisa.addEventListener("input", function () {
+    const texto = barraPesquisa.value.toLowerCase();
+
+    for (let i = 0; i < itens.length; i++) {
+      const item = itens[i].textContent.toLowerCase();
+      itens[i].style.display = item.includes(texto) ? "block" : "none";
+    }
+  });
+}
+
+document.getElementById("formLogin").addEventListener("submit", function (event) {
+  event.preventDefault(); // Impede envio automático
+
+  if (this.checkValidity()) {
+      // Validação passou, redireciona
+      window.location.href = "/site.html";
+  } else {
+      // Mostra mensagens de erro se campos estiverem inválidos
+      this.reportValidity();
   }
 });
